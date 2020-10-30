7a7bb2f106862387cc4b6b0545cfa68d79480d0b
James Fulford <james.fulford@outlook.com>
2020-10-29 21:16:19 -0400

    WIP: moving to same directory

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'src.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'src.wsgi.application'
 
 
 # Database
