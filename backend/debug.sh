a2cdd79b1597c73d5b3f9db666c9ad2f71a5355f
alejandrojsuazohvd <als5289@g.harvard.edu>
2020-11-02 18:33:26 -0600

    No more compose

--- backend/debug.sh
+++ backend/debug.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker-compose up --build
+echo "Waiting for debugger attachment..."
+docker run --name moneywise-backend-container-debug  --rm -it -p 8000:8000 -p 3000:3000 moneywise-backend
