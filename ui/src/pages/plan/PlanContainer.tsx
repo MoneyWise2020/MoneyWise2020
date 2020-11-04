8ea5389940c8e4289b4b443a648177fdc70dd21a
James Fulford <james.fulford@outlook.com>
2020-11-04 02:05:40 -0500

    WIP(ui): rules container, basic structure

---
+++ ui/src/pages/plan/PlanContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import { RulesContainer } from './rules/RulesContainer';
+
+export const PlanContainer = () => {
+    return <Container>
+        <RulesContainer />
+    </Container>
+}
