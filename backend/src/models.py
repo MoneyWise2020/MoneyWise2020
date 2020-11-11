82f73830dd6f4a1ad1c57023acb140957f1b3d4e
James Fulford <james.fulford@outlook.com>
2020-11-11 15:22:45 -0500

    fix(backend): frequency can be large, need more characters

--- backend/src/models.py
+++ backend/src/models.py
@@ -5,7 +5,7 @@ import uuid
 class Rule(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50, default="")
-    frequency = models.CharField(max_length=50, default="")
+    frequency = models.CharField(max_length=1024, default="")
     amount = models.DecimalField(max_digits=19, decimal_places=2, default=0)
    
     def __str__(self):
