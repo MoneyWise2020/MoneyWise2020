40b01b28445d8d569ccbf73c5ebbdfe1c45b697d
James Fulford <james.fulford@outlook.com>
2020-11-11 15:20:30 -0500

    fix(backend): restful urls

--- backend/src/urls.py
+++ backend/src/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('api/ping', views.hello_world),
     path('api/transactions', views.process_transactions),
-    path('api/rules_handler', views.rules_handler),
-    path('api/rules_by_id_handler/<str:rule_id>', views.rules_by_id_handler),    
+    path('api/rules', views.rules_handler),
+    path('api/rules/<str:rule_id>', views.rules_by_id_handler),    
 ]
