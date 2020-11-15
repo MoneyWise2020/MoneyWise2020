16eb93cb4f0f0ffcc00380268c6de492584049e0
alejandrojsuazohvd <als5289@g.harvard.edu>
2020-11-15 09:52:58 -0600

    Removing run script as CICD handles this now.

--- backend/run_jenkins.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp /usr/share/jenkins/backend-variables.env . 
-docker stop moneywise-backend-container
-docker rm -v moneywise-backend-container
-docker-compose -f docker-compose.yml -f docker-compose.override.prod.yml up -d
