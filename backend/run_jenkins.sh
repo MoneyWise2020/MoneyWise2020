8233ad14162b1c12451f046e3300ba096d3334b9
alejandrojsuazohvd <als5289@g.harvard.edu>
2020-11-15 09:47:39 -0600

    Adding env file copy

--- backend/run_jenkins.sh
+++ backend/run_jenkins.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cp /usr/share/jenkins/backend-variables.env . 
 docker stop moneywise-backend-container
 docker rm -v moneywise-backend-container
 docker-compose -f docker-compose.yml -f docker-compose.override.prod.yml up -d
