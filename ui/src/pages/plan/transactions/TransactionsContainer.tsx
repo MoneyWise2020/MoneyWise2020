010110504c18866a3f07751b95d5d7875be1e616
Sarab A <sarabjotanand@gmail.com>
2020-11-22 10:45:40 -0500

    Created a component for transactions and cleaned up unsused imports

--- ui/src/pages/plan/transactions/TransactionsContainer.tsx
+++ ui/src/pages/plan/transactions/TransactionsContainer.tsx
@@ -1,16 +1,9 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { IApiTransaction, TransactionsService } from './transactions-service';
-import { Currency } from '../../../components/currency/Currency';
+import { Transaction } from './Transaction'
 
 // TODO: get from login
 const userid = 'test'
-
-const style = {
-    height: 30,
-    border: "1px solid green",
-    margin: 6,
-    padding: 8
-  };
 
 export const TransactionsContainer = () => {
     const [data, setData] = useState<IApiTransaction[] | 'error'>();
@@ -45,13 +38,7 @@ export const TransactionsContainer = () => {
             </thead>
             <tbody>
                 {tableData.map(
-                        (transaction) => <tr key={transaction.id} className={transaction.calculations.balance > 0 ? 'table-success' : 'table-danger'}>
-                            <td><span className="text-nowrap">{transaction.day}</span></td>
-                            <td>{transaction.rule_id}</td>
-                            <td><Currency value={transaction.value} /></td>
-                            <td><Currency value={transaction.calculations.balance} /></td>
-                            <td><Currency value={transaction.calculations.working_capital} /></td>
-                        </tr>
+                        (transaction) => <Transaction transaction={transaction} />
                     )
                 }
             </tbody>
