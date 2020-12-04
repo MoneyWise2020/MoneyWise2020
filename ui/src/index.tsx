8ca3c58d432e3cb97ee257ba1b89c61920c98d87
James Fulford <james.fulford@outlook.com>
2020-12-04 00:21:37 -0500

    fix(ui): logout redirect

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -15,6 +15,7 @@ ReactDOM.render(
       domain={domain}
       clientId={clientId}
       redirectUri={window.location.origin}
+      returnTo={window.location.origin}
       cacheLocation="localstorage"
       useRefreshTokens
     >
